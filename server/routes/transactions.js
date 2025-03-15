bb47e9b723b3ad19757784ae282b5197b7d2118b
MdAqib184 <mdaqib184@gmail.com>
2025-03-15 21:02:55 +0530

    adding new feature

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -32,4 +32,38 @@ router.post('/', async (req, res) => {
     }
 });
 
+// Delete transaction
+router.delete('/:id', async (req, res) => {
+    try {
+        // Check if the ID is valid MongoDB ObjectId
+        const isMongoId = /^[0-9a-fA-F]{24}$/.test(req.params.id);
+        
+        let transaction;
+        if (isMongoId) {
+            // If it's a valid MongoDB ObjectId, try to find by _id
+            transaction = await Transaction.findOne({
+                _id: req.params.id,
+                userId: req.user._id
+            });
+        } else {
+            // If it's not a valid MongoDB ObjectId, try to find by the custom id field
+            transaction = await Transaction.findOne({
+                id: req.params.id,
+                userId: req.user._id
+            });
+        }
+        
+        if (!transaction) {
+            return res.status(404).json({ message: 'Transaction not found' });
+        }
+        
+        // Remove the transaction using the correct ID
+        await Transaction.findByIdAndDelete(transaction._id);
+        res.json({ message: 'Transaction deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting transaction:', error);
+        res.status(500).json({ message: error.message });
+    }
+});
+
 module.exports = router;
